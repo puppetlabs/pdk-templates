04f1d88f0308ddafa308d6d978f18b1293cd18bf
David Schmitt <david.schmitt@puppet.com>
2017-06-14 22:04:54 +0100

    (SDK-276) re-do rubocop configuration
    This uses hardcoded rubocop_defaults to build a minimal .rubocop.yml
    config. The configuration is minimal (only list what you want), and can
    be easily overridden.
    
    The profile_builder.rb script can help in rebuilding the rubocop_defaults
    file for new rubocop versions.
    
    The cleanup_cops.yml contains a list of basic cops everyone should check.
    This was originally derived from the main PDK rubocop configuration.
    See https://github.com/puppetlabs/pdk/pull/78.
    
    Downstream users can choose from four different strictness
    profiles: off, cleanups_only, strict, hardcore.
    
    * off: no cops enabled, use at your own risk.
    * cleanups_only: a set of uncontroversial cops that will cleanup your
      codebase nicely. All of them have autocorrect enabled and are safe to
      apply.
    * strict: the default. A good mix of cops following the established best
      practices of the community. The work required to pass this, will make
      your code base so much better.
    * hardcore: enables all rubocop default cops. Still uses the recommended
      configs from strict, but enabled additional, unvetted tests. Have fun!

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'rubocop', '= 0.49.1'
+gem 'rubocop-rspec', '= 1.15.1'
---
+++ rubocop/profile_builder.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'rubocop/version'
+
+File.delete('.rubocop.yml') if File.exist?('.rubocop.yml')
+
+default_configs = YAML.load(`rubocop --show-cops --require rubocop-rspec`)
+all_cops = default_configs.keys - [ 'AllCops', 'require' ]
+default_enabled_cops = all_cops.find_all { |c| default_configs[c]['Enabled'] }
+default_disabled_cops = all_cops - default_enabled_cops
+
+$stderr.puts "Found #{default_enabled_cops.length} enabled, and #{default_disabled_cops.length} disabled cops in the default config."
+
+# fetch config from current PDK
+FileUtils.cp("../../pdk/.rubocop.yml", '.')
+# stub out the included config to avoid rubocop complaints
+File.open('.rubocop_todo.yml', 'w') do |f|
+  # nothing
+end
+config = YAML.load(File.read('.rubocop.yml'))
+
+configured_cops = YAML.load(`rubocop --show-cops`)
+config_enabled_cops = all_cops.find_all { |c| configured_cops[c]['Enabled'] }
+config_disabled_cops = all_cops - config_enabled_cops
+
+$stderr.puts "Found #{config_enabled_cops.length} enabled, and #{config_disabled_cops.length} disabled cops in the recommended config."
+
+# The cleanups_only profile only has the uncontroversial cops enabled, and configred, everything else is disabled
+
+cleanup_cops = YAML.load(File.read('cleanup_cops.yml'))
+
+cleanup_enabled_cops = cleanup_cops - config_disabled_cops
+cleanup_disabled_cops = all_cops - cleanup_enabled_cops
+
+$stderr.puts "Found #{cleanup_enabled_cops.length} enabled, and #{cleanup_disabled_cops.length} disabled cops in the cleanup config."
+
+force_disabled_cops = config_disabled_cops - default_disabled_cops
+
+# all_cops.each do |c|
+#   if default_enabled
+#     if disabled
+#       # render
+#     end
+#   else
+#     if enabled
+#       # render
+#     end
+#   end
+# end
+File.open("defaults-#{RuboCop::Version.version}.yml", 'w') do |f|
+  f.puts YAML.dump(default_enabled_cops: default_enabled_cops)
+end
+
+puts YAML.dump(config_enabled_cops - cleanup_enabled_cops)
