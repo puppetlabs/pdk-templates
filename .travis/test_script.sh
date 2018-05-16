8cbaa15ec81d417555053e6689909417e4437340
Nick Walker <nick.walker@puppetlabs.com>
2018-05-15 17:13:51 -0700

    Disable sudo in travis testing to reduce boot time

--- .travis/test_script.sh
+++ .travis/test_script.sh
@@ -1,12 +1,3 @@
-
-echo 'setup puppetlabs apt repo'
-wget https://apt.puppetlabs.com/puppet5-release-trusty.deb
-sudo dpkg -i puppet5-release-trusty.deb
-sudo apt-get update
-
-echo 'install pdk'
-sudo apt-get install pdk
-
 PR_DIR=$PWD
 
 pdk new module pr_new_modules --template-url=$PR_DIR --skip-interview
